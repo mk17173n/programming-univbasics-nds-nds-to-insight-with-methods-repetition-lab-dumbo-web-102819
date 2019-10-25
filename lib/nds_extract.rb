9dcf4b087254e9bde13048bd0e1387471d134ea2
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 01:43:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,6 +43,18 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  totals = directors_totals(source)
+  dlist = list_of_directors(source)
+  
+  total = 0
+  
+  dlist.each do |name|
+    total += totals[name]
+  end
+  
+  total
+  
 end
 
 
